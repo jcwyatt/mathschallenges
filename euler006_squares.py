d7d80598df9fbc5c27cc5e36a18479a8bbd437cc
Jason Wyatt <jwyatt043@googlemail.com>
2017-10-18 01:42:36 +0100

    Add files via upload

---
+++ euler006_squares.py
@@ -0,0 +1,14 @@
+sumSqrs=0
+sqrSum=0
+for i in range (1,101):
+    sumSqrs += i*i
+    sqrSum += i
+
+print("Sum of Squares: ",sumSqrs)
+print("Square of Sum: ",sqrSum**2)
+
+print("Difference = ",sqrSum**2 - sumSqrs)
+
+
+    
+
