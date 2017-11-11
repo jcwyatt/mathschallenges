a72f2062bd538deef9c6398ff142346a03da3a96
jcwyatt <jwyatt043@googlemail.com>
2017-11-11 22:48:13 +0000

    2 parts done

--- euler011_largest_product_in_a_grid.py
+++ euler011_largest_product_in_a_grid.py
@@ -1,4 +1,6 @@
 import re
+from time import sleep
+
 with open('euler011numbers.txt') as f:
 	rawgrid = f.read()
 
@@ -7,3 +9,22 @@ strgrid = re.split('\W',rawgrid)
 grid = [int(number) for number in strgrid]
 
 print (grid[0], grid[20])
+
+biggestProduct = 0
+
+for locn,num in enumerate (grid):
+	#vertical rows
+	if locn<340:
+		currentProduct = num*grid[locn+20]*grid[locn+40]*grid[locn+60] 
+		if currentProduct>biggestProduct:
+			biggestProduct = currentProduct
+		print(biggestProduct)
+	#backslashes
+	if locn%20<17 and locn<340:
+		currentProduct = num*grid[locn+21]*grid[locn+42]*grid[locn+63] 
+		if currentProduct>biggestProduct:
+			biggestProduct = currentProduct
+		print(locn, num)
+		print(biggestProduct)
+		
+	#fwdslashes
